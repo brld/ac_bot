a353bb33d01fdef88310007b20342ee963d13220
Hum4n01d <hum4n01d@icloud.com>
2018-08-14 09:15:10 -0700

    Changed repost_suggestions channel id

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const { join } = require('path')
 const { readFileSync } = require('fs')
 
 module.exports = {
-  suggestionChannelID: '463395823959408640',
+  suggestionChannelID: '478687396896964609',
   leaderboardChannelID: process.env.DEBUG ? '467777176570429470' : '467777046374907914',
   infoText: readFileSync(join(__dirname, 'infoText.txt'), 'utf-8'),
   abbreviations: [
